a4408c06ddfaaad2b1db6c9e5d701513209cf825
pambonian <pambonian@gmail.com>
2021-12-20 19:36:21 -0500

    seperation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+// import game
+import Game from './Game'
+
+class App extends Component {
+    render() {
+        return (
+            <Game />
+        );
+    }
+}
+
+export default App;
